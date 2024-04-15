42663f3670ec29565fdaf0fe0ef10e159a795bf0
Andreasgura <156321321+Andreasgura@users.noreply.github.com>
2024-04-15 14:17:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script>
+  import HeaderComponent from './assets/components/HeaderComponent.vue'
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
