667ae0246df4650e6d1f9575154283abbfdbf6c6
Andreasgura <andre-56@hotmail.it>
2024-04-15 18:05:16 +0200

    bonus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <HeaderComponent />
-  <MainComponent />
+  <div class="apiloader d-flex justify-content-center align-items-center"  
+  v-if="loading">
+    <i class="fa-solid fa-spinner fa-spin"></i>
+  </div>
+  <div v-else>
+    <HeaderComponent />
+    <MainComponent />
+  </div>
 </template>
 
 <script>
@@ -17,13 +23,19 @@
     data(){
       return {
         store,
+        loading: true
       }
     },
     methods: {
       apiRequest(){
+        this.loading = true
         axios.get(this.store.apiUrl).then((response) => {
           console.log(response.data);
           this.store.characters = response.data.data
+        }).catch((error) => {
+          console.log(error);
+        }).finally(() => {
+          this.loading = false
         })
       }
     },
@@ -34,5 +46,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+.apiloader{
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  font-size: 4rem;
+  background-color: rgba(0, 0, 0, 0.5);
+}
 </style>
