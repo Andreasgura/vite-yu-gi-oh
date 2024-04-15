e86221d835c346bba5440bb479ed4ade3aa9b053
Andreasgura <andre-56@hotmail.it>
2024-04-15 15:01:53 +0200

    components structure

---
+++ src/assets/components/CardsWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container p-4 as">
+        <div class="top-wrapper">
+            top wrapper
+        </div>
+        <div class="main-wrapper container ">
+            <div class="row">
+                <div class="col-12 col-md-6 col-5 p-3">
+                    <CardComponent />
+                </div>
+            </div>   
+        </div>
+    </div>
+</template>
+
+<script>
+    import CardComponent from './CardComponent.vue'
+    export default {
+        name: 'CardsWrapper',
+        components: {
+            CardComponent
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+ .container .as {
+    background-color: white;
+
+    .top-wrapper {
+        background-color: $cardcontainerbg;
+        color: white;
+    }
+ 
+ }
+</style>
---
+++ src/assets/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <div class="container">
+            <CardsWrapper />
+        </div>
+    </main>
+</template>
+
+<script>
+    import CardsWrapper from './CardsWrapper.vue'
+    export default {
+        name: 'MainComponent',
+        components: {
+            CardsWrapper,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+main {
+    background-color: $mainbg;
+}
+</style>
